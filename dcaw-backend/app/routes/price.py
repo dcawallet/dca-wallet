9991dcf5389be6f80c3a4a9f1ac3e9521c5c5d97
Joao Bizzo <brandtjoao7@gmail.com>
2025-09-24 09:33:01 -0300

    Fixed bitcoin price path in price.py

--- dcaw-backend/app/routes/price.py
+++ dcaw-backend/app/routes/price.py
@@ -10,7 +10,7 @@ async def get_bitcoin_price():
     Fetches the latest Bitcoin price data (USD, BRL, and calculated USD/BRL rates)
     from the cached `bitcoin_price.json` file.
     """
-    file_path = "/home/joaobizzo/Documents/code/dcawallet/dca-wallet/dca-backend/bitcoin_price.json"
+    file_path = "bitcoin_price.json"
     if not os.path.exists(file_path):
         raise HTTPException(status_code=404, detail="Bitcoin price data not found.")
     try:
